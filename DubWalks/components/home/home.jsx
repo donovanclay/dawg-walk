def358c344136b3d12f8a096b56d61a8e46f61be
zylim1128 <zylim@uw.edu>
2023-10-14 15:15:38 -0700

    Added constants

---
+++ DubWalks/components/home/home.jsx
@@ -0,0 +1,50 @@
+import { useState}  from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from 'react-native'
+import { useRouter } from 'expo-router';
+
+import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants';
+
+// define the job types
+// const jobTypes = ["Full-time", "Part-time", "Contractor"];
+
+const Welcome = ( { searchTerm, setSearchTerm, handleClick }) => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-time');
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.welcomeMessage}> CIRCLE </Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TouchableOpacity style={styles.searchBtn} onPress={(handleClick) => {}}>
+            <Image
+              source={icons.search}
+              resizeMode='contain'
+              style={styles.searchBtnImage}
+            />
+          </TouchableOpacity>
+            <TextInput
+              style={styles.searchInput}
+              // parse in the value of what you searching for
+              value={searchTerm}
+              onChangeText={(text) => setSearchTerm(text)}
+              maxLength={23}
+              placeholder="Search event"
+            />
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default Welcome;
