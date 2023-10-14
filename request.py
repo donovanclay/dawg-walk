3010a598ffb0738bc5d8d16d2fde45cbecbc5082
Donovan Clay <donoclay@cs.washington.edu>
2023-10-14 15:53:15 -0700

    create DawgWalkRequest class

--- request.py
+++ request.py
@@ -1,11 +1,16 @@
-class Request:
+class DawgRequest:
 
     # FIELDS
     #
     # name:
     # destination:
+    # meeting_location
     # arrival_time:
     # depart_time:
 
-    def __init__(self, name, destination, arrival_time):
-        print("hello")
+    def __init__(self, name, destination, meeting_location, arrival_time, depart_time):
+        self.name = name
+        self.destination = destination
+        self.meeting_location = meeting_location
+        self.arrival_time = arrival_time
+        self.depart_time = depart_time
