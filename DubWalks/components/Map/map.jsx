d587be2b37eb76b8595ac83dd4c82da5d72cf1f2
zylim1128 <zylim@uw.edu>
2023-10-14 20:42:30 -0700

    What the ehck

---
+++ DubWalks/components/Map/map.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+const MapComponent = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 47.6546,
+          longitude: -122.3075,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        zoomEnabled = {true}
+        minZoomLevel={14}
+        maxZoomLevel={20}
+      />
+      <Marker
+          coordinate={{ latitude: 47.65318, longitude: -122.30499 }}
+          title={"Marker Title"}
+          description={"Marker Description"}
+        />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default MapComponent;
